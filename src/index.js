3204cf1bb6ad278ced185148d8c78f909c31a82d
Shawn <shawn.w8465@gmail.com>
2019-02-16 11:06:04 -0700

    Looking good!
    Javascript is working correctly
    
    Better than I expected

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'popper.js/dist/popper.js';
+import 'jquery/dist/jquery.js';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import Home from './Home';
-
-import 'bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
