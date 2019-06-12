861511c771262f898b6a3f6bd981365687304eea
Cloaking_Ocean <shawn.w8465@gmail.com>
2019-06-12 02:19:36 +0000

    Converts Music.html into JSX

--- src/js/App.js
+++ src/js/App.js
@@ -5,6 +5,7 @@ import Java from './Java';
 import Unity from './Unity';
 import Web from './Web';
 import Gaming from './Gaming';
+import Music from './Music';
 
 class App extends Component {
     
---
+++ src/js/Music.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+class Music extends Component {
+    
+    render() {
+        return (
+            <div>
+                {/* Breakpoint debugging tool to be filled in with javascript from writeBreakpoint() method */}
+                <header className="header" data-id="5">
+                    {/* To be filled in with javascript from writeHeader() method */}
+                </header>
+        
+                <div className="container">
+                    <div className="row">
+                        <h1>Music | Pleasant Vibrations</h1>
+                    </div>
+                    <section>
+                        <h2>Music Introduction</h2>
+                        <p>
+                            Since about 5th grade, I've been able to play the piano! I've practiced on and off throughout a couple of years!
+                            With the help of my school and my grandmother, I've been able to improve my piano skills quite drastically from when I first
+                            started. However, even before learning to play the piano, music was a huge part of my life! I listen to music almost any
+                            chance I get!
+                        </p>
+                    </section>
+                    <section>
+                        <h2>Learning the Piano</h2>
+                        <p>
+                            My first piano experience started with my grandmother. She taught me very simple and short songs at the beginning like Mary
+                            Had a Little Lamb. I began to feel like I was cheating learning because instead of translating notes into their letters
+                            and then finding the key corresponding to that letter, I would just look at how many keys away a note was and just play
+                            it that way. I thought I would never be able to improve my skills with the piano because of that. However, my father and grandmother continued
+                            to push me and encourage me. Eventually, I found that I was actually playing the notes correctly rather than my previous method!
+                        </p>
+        
+                        <p>
+                            My grandmother taught me mainly classical music and hymns. I learned to play songs from Bach, Beethoven, Mozart, and more! It
+                            was quite exciting to learn how to perform such classics from the beginning! Attending a piano class after school, I also learned
+                            many lullabies and modern songs! Having this mix of genres made it exciting to play and learn more! Recently, I've continued
+                            to learn hymns as well as learning many popular pop songs!
+                        </p>
+                    </section>
+        
+                    <section>
+                        <h2>Music I Enjoy</h2>
+                        <p>
+                            Being a religious person, I've always loved hymns and songs similar to them! I also found myself being quite content with many
+                            different genres. My mood dictates what style I desire. One of my favorite bands, when I was younger, was The Black Eyed
+                            Peas. Over the years I've highly enjoyed songs from Panic at the Disco, Secondhand Serenade, Maroon 5, Paramore, Echosmith,
+                            Coldplay and more! The genres I don't enjoy as much are rap and screamo music.
+                        </p>
+        
+                        <p>
+                            Recently I've also enjoyed many electronic songs from TheFatRat and Alan Walker! I've started enjoying the Dubstep and EDM genres
+                            more and more as I've explored their music. I also really enjoy chill genres like Cavetown and Khai Dreams. Music is a huge part
+                            of my life!
+                        </p>
+                    </section>
+                </div>
+            </div>
+        )
+    }
+        
+}
+export default Music;
