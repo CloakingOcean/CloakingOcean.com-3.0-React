3af60b4411b23bc1cf98f66b7b7cdeb54002a6b1
Cloaking_Ocean <shawn.w8465@gmail.com>
2019-06-17 19:46:07 +0000

    Removes html link from logo
    - Replaces html link with <Link> from ReactRouter

--- src/js/navigation/Logo.js
+++ src/js/navigation/Logo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import logoImg from "../../images/logo/logo-white.svg";
+import { Link } from 'react-router-dom';
 
 class Logo extends Component {
 
@@ -8,7 +9,7 @@ class Logo extends Component {
         return (
             <div className='container-fluid'>
                 <div className='navbar-header'>
-                    <a href='index.html'><img className='logo navbar-brand' src={logoImg} alt='Cloaking Ocean Logo'/></a>
+                    <Link to="/"><img className='logo navbar-brand' src={logoImg} alt='Cloaking Ocean Logo'/></Link>
                 </div>
             </div>
         );
