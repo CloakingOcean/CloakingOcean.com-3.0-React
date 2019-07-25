60411b77492b7ead537cd75aae1d7a9a53dfc281
Cloaking_Ocean <shawn.w8465@gmail.com>
2019-07-25 16:52:36 +0000

    Attempts to resolve unexpected bugs with css media queries

--- src/js/navigation/Navigation.js
+++ src/js/navigation/Navigation.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleResponsiveClass() {
+    var x = document.getElementById("topNav");
+    if (!x.classList.contains("responsive")) {
+        x.className += " responsive";
+    } else {
+        x.classList.remove("responsive");
+    }
+}
+
 class Navigation extends Component {
+
+
 
     render() {
         return (
-            <div id="topNav" className='container-fluid' width='100vw'>
+            <div id="topNav" className="container-fluid topNav" width="100vw">
                 <ul className='navbar-nav justify-content-end ml-auto mr-auto ml-sm-auto mr-sm-0'>
                     {this.props.linkList}
                 </ul>
                 
-                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                    <i class="fa fa-bars"></i>
+                <a href="javascript:void(0);" className="icon" onClick={toggleResponsiveClass}>
+                    <i className="fa fa-bars"></i>
                 </a>
             </div>
         );
