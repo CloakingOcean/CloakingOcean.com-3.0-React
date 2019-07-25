4f9e590366bc90904a20fd3df637c64fdb373b74
Cloaking_Ocean <shawn.w8465@gmail.com>
2019-07-25 16:11:27 +0000

    Adds initial html and mobile based css
    Adding media queries in next commit

--- src/js/navigation/Navigation.js
+++ src/js/navigation/Navigation.js
@@ -4,10 +4,14 @@ class Navigation extends Component {
 
     render() {
         return (
-            <div className='container-fluid' width='100vw'>
+            <div id="topNav" className='container-fluid' width='100vw'>
                 <ul className='navbar-nav justify-content-end ml-auto mr-auto ml-sm-auto mr-sm-0'>
                     {this.props.linkList}
                 </ul>
+                
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+                </a>
             </div>
         );
     }
